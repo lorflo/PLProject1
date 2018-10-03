03d64471a8b259d2039af994faa90f910efa1af7
adrian <adrian@Ay-drian>
2018-10-03 16:55:20 -0600

    Under new folder and index.php I chanches the if statemnets into switch case statements for cleaner and readable perposes.

--- src/new/index.php
+++ src/new/index.php
@@ -5,6 +5,7 @@ $pid = uniqid();
 $response = null; 
 $strat = null;
 
+/*
 if(isset($_GET['strategy']))
 {
     $strat = $_GET['strategy'];
@@ -26,7 +27,23 @@ if($strat == null)//no Strategy
 {
     $response =  array('response' => false, 'reason' => "Strategy not specified");
 }   
-
+*/
+if (isset($_GET['strategy'])){
+    $strat = $_GET['strategy'];
+    switch ($strat) {
+        case 'Smart':
+            $response = array('response' => true, 'pid' => $pid);
+        break;
+        case 'Ranom':
+            $response = array('response' => true, 'pid' => $pid);
+        break;
+        default:
+            $response = array('response' => false, 'reason' => "Unknown strategy");
+        break;
+    }
+}
+else
+    $response = array('response' => false, 'reason' => "Strategy not specified");
 
 echo json_encode($response);
 
