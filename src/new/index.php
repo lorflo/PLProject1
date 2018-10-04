83a26d3d9d800bc0a436a67d2fa64512eef1283e
adrian <adrian@Ay-drian>
2018-10-03 19:24:44 -0600

    Hello

--- src/new/index.php
+++ src/new/index.php
@@ -5,36 +5,14 @@ $pid = uniqid();
 $response = null; 
 $strat = null;
 
-/*
-if(isset($_GET['strategy']))
-{
-    $strat = $_GET['strategy'];
-    if($strat == "Smart")
-    {
-        $response = array('response' => true, 'pid' => $pid);
-    }
-    elseif($strat == "Random")
-    {
-        $response =array('response' => true, 'pid' => $pid);
-    }
-    //for an unkown strategy
-    else
-    {
-        $response = array('response' => false, 'reason' => "Unknown strategy");
-    }
-}
-if($strat == null)//no Strategy
-{
-    $response =  array('response' => false, 'reason' => "Strategy not specified");
-}   
-*/
+
 if (isset($_GET['strategy'])){
     $strat = $_GET['strategy'];
     switch ($strat) {
-        case 'Smart':
+        case "Smart":
             $response = array('response' => true, 'pid' => $pid);
         break;
-        case 'Ranom':
+        case "Random":
             $response = array('response' => true, 'pid' => $pid);
         break;
         default:
